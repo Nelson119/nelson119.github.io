c463f1812b3c9656be7d705fe87dbf255a102fc6
NelsonTan <nelson.tan@hwaotech.com>
2025-05-23 17:26:34 +0800

    init 2

---
+++ components/Landing.vue
@@ -0,0 +1,23 @@
+<template></template>
+
+<script setup>
+  const router = useRouter();
+  // const route = useRoute();
+
+  const items = ref([]);
+  const isLoading = ref(false);
+  const error = ref(null);
+
+  // const signIn = () => {
+  //   router.push('/sign/in');
+  // };
+  // const signUp = () => {
+  //   router.push('/sign/up');
+  // };
+
+  onMounted(() => {});
+
+  // definePageMeta({
+  //   layout: 'default',
+  // });
+</script>
